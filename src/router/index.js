e358c9cda8c81423608401662330df6755301d88
rafaelrebello <rrebellov@gmail.com>
2024-07-30 10:30:41 -0300

    enviando atividade

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView,
     },
   ],
 })
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,3 +1,4 @@
+/*
 import axios from 'axios'
 
 const apiClient = axios.create({
@@ -12,5 +13,26 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get('/events')
+  }
+}
+  */
+
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  },
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
   }
 }
