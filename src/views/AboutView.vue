8185257d251de1fafff10189a28f491d6c35afab
rafaelrebello <rrebellov@gmail.com>
2024-08-04 16:48:02 -0300

    atualizando about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
     <h1>A site for events to better the world.</h1>
+
+    <h4>Site criado com base no tutorial Real World Vue, para a disciplina de Desenvolvimento Web do curso de Sistemas de Informação no Instituto Federal Catarinense</h4>
+    <h4>Repositório: <a href="https://github.com/rafaelrebello/real-world-vue">Github</a></h4>
+    <h4>Feito por: <a href="https://github.com/rafaelrebello">Rafael Rebello Vitorino</a></h4>
   </div>
 </template>
 
